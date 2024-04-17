5fa4700d78890e5d374956c58d750bf7c4a31112
Subham <subhamt1519@gmail.com>
2024-04-17 18:16:19 +0530

    All components visible in main file now

--- src/index.js
+++ src/index.js
@@ -12,12 +12,25 @@ import SearchFilter from "./SearchFilter";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <Counter /> */}
-    {/* <InputDisplay /> */}
-    {/* <CharacterCount /> */}
-    {/* <ToggleVisibility /> */}
-    {/* <ToDo /> */}
-    {/* <ColorSwitcher /> */}
-    {/* <SearchFilter /> */}
+    <h2>1. Counter</h2>
+    <Counter />
+    <hr />
+    <h2>2. Display Input</h2>
+    <InputDisplay />
+    <hr />
+    <h2>3. Input Character Count</h2>
+    <CharacterCount />
+    <hr />
+    <h2>4. Toggle Element Visibility</h2>
+    <ToggleVisibility />
+    <hr />
+    <h2>5. Todo List</h2>
+    <ToDo />
+    <hr />
+    <h2>6. Color Switcher</h2>
+    <ColorSwitcher />
+    <hr />
+    <h2>7. Filter Search</h2>
+    <SearchFilter />
   </React.StrictMode>
 );
